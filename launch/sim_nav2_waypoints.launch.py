47e847322100f5d9cf37ab1cfdb246487132630c
sriharsha-bullwork <sriharsha@bullworkmobility.com>
2025-09-01 09:52:37 +0000

    added a comment for demo

--- launch/sim_nav2_waypoints.launch.py
+++ launch/sim_nav2_waypoints.launch.py
@@ -7,6 +7,8 @@ from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
 
 
+# THIS IS A COMMENT
+
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='true')
